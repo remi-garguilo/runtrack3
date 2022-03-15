99cee199eb9558c623d31d516e7f83b2e89f0e60
remig13 <remi.garguilo@epitech.eu>
2022-03-15 11:37:23 +0100

    change name function

--- jour02/job03/script.js
+++ jour02/job03/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     var btncounter = document.querySelector('#button');
     let counter = 0;
 
-    btncounter.addEventListener('click', function showhide(){
+    btncounter.addEventListener('click', function addone(){
        counter++;
        document.querySelector('#compteur').innerHTML = counter;
     })
